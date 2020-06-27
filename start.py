b6ed5be560efdde39030698b592d18956421d293
Roza Chaikozova <r.chaikozova@gmail.com>
2020-06-27 11:54:53 +0600

    second try

--- start.py
+++ start.py
@@ -1,18 +1,38 @@
-from flask import Flask
-
-
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
+abouts = [
+    {
+        'name': 'Elena Chegibaeva',
+        'position': 'Co-founder'
+    },
+    {
+        'name': 'Nazira Sheraly',
+        'position': 'Co-founder'
+    },
+    {
+        'name': 'Jazgul',
+        'position': 'Coordinator'
+    }
+]
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('home.html')
 
 
-@app.route('/main')
-def main():
-    return 'main'
+@app.route('/news')
+def news_page():
+    text = 'Привет, я Роза'
+    return render_template('news.html', titel='News', text=text)
+
+
+@app.route('/about')
+def about_page():
+    return render_template('about.html',titel='About us', abouts=abouts)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port="800")
